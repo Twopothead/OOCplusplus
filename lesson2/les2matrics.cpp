bd757ff5adaf84f6ca47b1ce1fed064b9728a4e2
Twopothead <3103204417@qq.com>
2017-03-15 22:12:01 +0800

    Matrix

--- lesson2/les2matrics.cpp
+++ lesson2/les2matrics.cpp
@@ -14,21 +14,67 @@ typedef int* intMatrixptr;
 class DynamicMatrix
 {
 public:
-	DynamicMatrix();
-
+	DynamicMatrix(int M,int N);
+	~DynamicMatrix();
+	displayMatrix();
+	MulMatrix();
+	reflextionMatrix();
+	rotationMatrix();
+	transportMatrx();
+	TestMatrix();
 private:
 	int m,n;
-	int *matrix;
+	intMatrixptr *matrix;
 };
-DynamicMatrix(){
+DynamicMatrix::DynamicMatrix(int M,int N){
+	m=M;
+	n=N;
+	int i=0;
+	int j=0;
+	int count=0;
+	matrix=new intMatrixptr[m];
+	for(i=0;i<m;i++)
+			matrix[i]=new int[n];
 
-	
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			count++;
+			matrix[i][j]=count;
+		}
+	}
 }
+DynamicMatrix::displayMatrix(){
+	int i,j;
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++)
+			cout<<matrix[i][j]<<"\t";
+		cout<<endl;
+	}
+}
+DynamicMatrix::~DynamicMatrix()
+{
+	int i=0;
+	for(i=0;i<m;i++)
+		delete[] matrix[i];
+	delete []matrix;
+}
+DynamicMatrix::reflextionMatrix(){
 
+}
+DynamicMatrix::rotationMatrix(){
 
+}
+DynamicMatrix::transportMatrx(){
+
+}
+DynamicMatrix::TestMatrix(){
+	displayMatrix();
+
+}
 int main()
 {
-	
+	DynamicMatrix Mat(4,6);
+	Mat.TestMatrix();
 	return 0;
 
 }
